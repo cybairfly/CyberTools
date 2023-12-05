427e351f4152362fd5e756be101901a57cd22cbe
Tobey <cyberfly@email.cz>
2023-12-05 20:30:01 +0100

    fix: update entry point

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 /* eslint-disable import/no-relative-packages */
+import * as consts from './consts/lib';
 import { Error, errors } from './error/lib';
-// import { gears } from './gears/lib';
+import * as gears from './gears/lib';
 import { Human } from './human/lib';
-import { log } from './logger/lib';
+import { Logger } from './logger/lib';
 import { login } from './login/lib';
-// import { } from './server/lib';
-import { State } from './state/lib';
+// import { Server } from './server/lib';
+// import { State } from './state/lib';
 
 export {
-	State,
+	// State,
+	// Server,
+	Logger,
 	Human,
 	Error,
 	errors,
-	log,
 	login,
-	// gears,
+	gears,
+	consts,
 };
