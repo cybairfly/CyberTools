6704b7251fa0e2bc75704d2dcf4b6bdda3ce708b
Tobey <cyberfly@email.cz>
2023-12-05 20:43:54 +0100

    fix(error): convert entry point to esm

--- error/lib/index.js
+++ error/lib/index.js
@@ -1,6 +1,7 @@
-const { CyberError: Error } = require('./error');
-const { Errors } = require('./errors');
+import { CyberError } from './error';
+import { Errors } from './errors';
 
+const Error = CyberError;
 const errors = new Errors();
 
 module.exports = {
