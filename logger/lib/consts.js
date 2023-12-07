73a247aad1c7ad6d850be656dcdf44b8f92971b0
Tobey <cyberfly@email.cz>
2023-12-07 13:46:33 +0100

    feat(logger)!: rewrite & optimize logger
    backtrack - working custom generic, defaults extra
    
    backtrack - working before generic extensions
    
    logger/wip tests
    
    logger/working prototype
    
    finalize prototype
    
    support custom redactor to be null
    
    tests & cleanup

---
+++ logger/lib/consts.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-useless-computed-key */
+export const LEVEL_TO_COLOR = {
+	[1 /* ERROR */]: 'red',
+	[2 /* SOFT_FAIL */]: 'red',
+	[3 /* WARNING */]: 'yellow',
+	[4 /* INFO */]: 'green',
+	[5 /* DEBUG */]: 'blue',
+	[6 /* PERF */]: 'magenta',
+};
--- logger/lib/tools.js
+++ logger/lib/tools.js
@@ -1,23 +1,43 @@
-const deepTransform = (object, transformer, ...args) => {
-	Object.keys(object).forEach(key => {
-		transformer(object, key, ...args);
+export const DefaultLoggers = levels => logger => levels.map(level => logger[level]);
+export const CustomLoggers = levels => (logger, reducers = []) => levels.reduce(applyReducers(logger, reducers), {});
+export const extendDefaultLoggers = levels => (logger, customLoggers) => levels.forEach(level => logger[level] = customLoggers[level]);
 
-		if (object[key] && typeof object[key] === 'object')
-			return deepTransform(object[key], transformer, ...args);
+export const Reducers = redactor => ({
+	format: (...args) => [`${JSON.stringify(args[0], null, 4)}`],
+	redact: (...args) => args.map(arg => redactor ? redactor(arg) : arg),
+	custom: (...args) => typeof args[args.length - 1] === 'object' && !Array.isArray(args[args.length - 1]) ?
+		[`${args
+			.slice(0, -1)
+			.map(arg =>
+				typeof arg === 'object' ?
+					JSON.stringify(arg)
+					: arg)
+			.join(' ')}`,
+		args[args.length - 1],
+		] :
+		`${args
+			.map(arg =>
+				typeof arg === 'object' ?
+					JSON.stringify(arg) :
+					arg)
+			.join(' ')}`,
+});
+
+export const applyReducers = (logger, reducers = []) => (pool, level) => {
+	const levelLogger = logger[level];
+
+	return ({
+		...pool,
+		[level]: (...args) => {
+			const updatedArgs = reducers.reduce((args, next) => next(...args), args);
+
+			return Array.isArray(updatedArgs) ?
+				levelLogger?.call(logger, ...updatedArgs) :
+				levelLogger?.call(logger, updatedArgs);
+		},
 	});
-
-	return object;
 };
 
-const redactor = (object, key, redactKeys) => {
-	if (redactKeys.some(redactKey => key === redactKey))
-		object[key] = '<redacted>';
-};
-
-const redactObject = (object, transformer = redactor, redactKeys = ['proxyUrl', 'proxyUrls']) =>
-	deepTransform(object, transformer, redactKeys);
-
-export {
-	deepTransform,
-	redactObject,
-};
+export const getLevels = levelStrings => levelStrings
+	.filter(level => !['OFF', 'SOFT_FAIL'].includes(level))
+	.map(level => level.toLowerCase());
