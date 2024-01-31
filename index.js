31c1998e7347d994c3f5bec473c0991fbff9042a
Tobey <cyberfly@email.cz>
2024-01-31 09:50:52 +0100

    codex/support installs from repository

---
+++ index.js
@@ -0,0 +1,20 @@
+import * as consts from 'cyber-consts';
+import { Error, errors } from 'cyber-error';
+import { Human } from 'cyber-human';
+import { login } from 'cyber-lobby';
+import { Logue } from 'cyber-logue';
+import * as tools from 'cyber-tools';
+// import { Server } from './server/lib';
+// import { State } from './state/lib';
+
+export {
+	// State,
+	// Server,
+	Error,
+	Human,
+	Logue,
+	errors,
+	login,
+	tools,
+	consts,
+};
